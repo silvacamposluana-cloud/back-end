325a220ff4669983ec377dbbf2181afc23aea67d
silvacamposluana-cloud <silva.campos.luana@escola.pr.gov.br>
2026-03-05 11:51:32 -0300

    Create cont1.py

---
+++ conteudo/cont1.py
@@ -0,0 +1,5 @@
+#int para numeros inteiros
+#float para numeros reais ou com virgula
+#bool para valores logicos de verdeiro ou falso
+#str para strings que so palavras caracteres ou textos
+#para saber o tipo de uma variavel pode se usar comando print
