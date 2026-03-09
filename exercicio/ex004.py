f823d914492282eef158a0dbb3e77c94873a2512
silvacamposluana-cloud <silva.campos.luana@escola.pr.gov.br>
2026-03-09 08:26:07 -0300

    Create ex004.py

---
+++ exercicio/ex004.py
@@ -0,0 +1,7 @@
+num1 = int(input('digite um numero'))
+num2 = int(input('digite outro numero'))
+
+soma= num1 + num2
+
+#print('a soma entre {}' .format(num1), 'é {}' .format(num2), 'é {}' .format(soma))
+print('a soma entre {} e {} é {}'.format(num1,num2,soma))
