a09befc7c839878fee593a02c8f406bdf506c802
silvacamposluana-cloud <silva.campos.luana@escola.pr.gov.br>
2026-03-11 11:30:30 -0300

    Create cont3.py

---
+++ conteudo/cont3.py
@@ -0,0 +1,10 @@
+n = input('digite algo')
+print(n.isnumeric())# para perguntar se é numero
+print(n.isalpha())# para perguntar se é letra
+print(n.isalnum())# para perguntar se é letra ou numero
+print(n.isupper())# para perguntar se é só com letras maiusculas
+print(n.isdecimal())# para verificar se é decimal
+print(n.isdigit())# para verificar se é digito
+print(n.islower())# para verificar se é apenas letras minusculas
+print(n.isidentifier())# para verificar se é um identificador valido
+print(n.isspace())# para verificar se tem um espaço em branco
