0bb0228e719e22c439eab2cf9f3dd96dd5429dd4
silvacamposluana-cloud <silva.campos.luana@escola.pr.gov.br>
2026-03-11 11:48:40 -0300

    Create cont4.py

---
+++ conteudo/cont4.py
@@ -0,0 +1,7 @@
+.0 + é utilizado como operador de adição 
+.0 - é utilizado como sinal de subtração
+.0 * é utilizado para fazer a multiplicação
+./ é utilizada para fazer divisão
+.** é utilizado para fazer a exponenciação
+.// é utilizado para fazer a divisão inteira
+ .% é utilizada para o resto da divisao
