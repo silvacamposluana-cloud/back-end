497c6831f49fd6f81589c5e4e30cce1f09a95e9b
silvacamposluana-cloud <silva.campos.luana@escola.pr.gov.br>
2026-03-16 11:01:49 -0300

    Create cont5.py

---
+++ conteudo/cont5.py
@@ -0,0 +1,15 @@
+Ordem de precedência
+
+.definição:é a ordem na qual os operadores serão executados
+.1 expressões dentro de parenteses
+.2 em seguida vem as potenciações
+.3 em terceiro lugar vem a multiplicação,a divisão normal,a divisão inteirs e o resto da divisão. no caso de ter mais de um deles você resolve quem aparecer primeiro
+.4 e por ultimo a soma e a subtração
+
+Exemplo
+
+.5+3*2=
+.3*5+4**2=
+.3*(5+4)**2=
+
+.O python é utilizado em cálculos cientificos é o tamanho da memoria ou seja conseguimos chegar a números extremamente grandes
