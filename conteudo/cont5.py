97aed3f4a5a1a8036b9f76b931fc1a9300dd5c06
silvacamposluana-cloud <silva.campos.luana@escola.pr.gov.br>
2026-03-19 07:50:00 -0300

    Update cont5.py

--- conteudo/cont5.py
+++ conteudo/cont5.py
@@ -13,3 +13,10 @@ Exemplo
 .3*(5+4)**2=
 
 .O python é utilizado em cálculos cientificos é o tamanho da memoria ou seja conseguimos chegar a números extremamente grandes
+
+Outros operadores
+
+Para exponenciação podemos usar o pow(x,y) onde o número x é elevado aou número y
+Para raiz quadrada podemos usar o **(1/2), elevando o número a 1/2 nos obtemos a raiz quadrada, para raiz cúbica **(1/3), e assim por diante.
+Podemos usar os operadores em strings
+Ex '='*5 vai ficar '====='
