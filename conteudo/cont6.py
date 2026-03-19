6c6e4c4147adc1fdd0e901b5582925135515283b
silvacamposluana-cloud <silva.campos.luana@escola.pr.gov.br>
2026-03-19 08:01:22 -0300

    Create cont6.py

---
+++ conteudo/cont6.py
@@ -0,0 +1,27 @@
+Operadores
+
+.podemos escrever um resultao na quantidade de caracteres epecificar ao colocar o valor dentro da máscara após escrever
+
+nome= igual ('qual é seu nome')
+print ('prazer em conhece-lo {:20}!. format(nome))
+
+       ex:
+       qual é seu nome?gustavo
+       prazer em conhece-lo gustavo
+
+       ALINHAMENTO DE MÁSCARA
+       
+Podemos alinhar o nome dentro do número de caracteres que desejamos
+Programaono (EM 12
+Pence De Durtes 2 Ano
+Alinhamento para direita
+print('prazer em conhece-lo {:>20}!'.format(nome))
+Programece Front-Enit-2
+Alinhamento para a esquerda
+nome input ('Qual é o seu nome?')
+print('prazer em conhece-lo {:<20}!'.format(nome))
+MANG TH
+Alinhamento no centro
+nome input ('Qual é o seu nome?')
+nome = input ('Qual é o seu nome?')
+print('prazer em conhece-lo {:^20}!'.format(nome))
