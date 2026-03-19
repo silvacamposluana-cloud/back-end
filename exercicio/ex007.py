adb5db13be4cbc1204ae0fb47636528628f5b361
silvacamposluana-cloud <silva.campos.luana@escola.pr.gov.br>
2026-03-19 08:10:48 -0300

    Create ex007.py

---
+++ exercicio/ex007.py
@@ -0,0 +1,4 @@
+n1 = int (input ('Digite um número '))
+print('o número digitado foi {} o seu dobro é {}, seu triplo {} e sua raiz quadrada é {:.2f}
+'.format(n1, n1*2, n1*3, n1**0.5))
+#para calcular a raiz quadrada posso utilizara função pow(n**0.5)
