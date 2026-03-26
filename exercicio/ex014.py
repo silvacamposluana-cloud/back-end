d73e9e13b2d61a60917a1a42c5e22655bcc14b39
silvacamposluana-cloud <silva.campos.luana@escola.pr.gov.br>
2026-03-26 07:53:31 -0300

    Create ex014.py

---
+++ exercicio/ex014.py
@@ -0,0 +1,4 @@
+RESOLUÇÃO
+salario float (input('Qual é o salário do funcionário?R$'))
+aumento= salario + (salario*15/100)
+print('um funcionário que ganhava R${:.2f), com 15% de aumento, passa a receber R${:.2}'.format(salario, aumento))
