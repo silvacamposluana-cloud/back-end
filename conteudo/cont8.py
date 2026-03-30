b5f030e7d17a83252390d820da236c5a1edb113b
silvacamposluana-cloud <silva.campos.luana@escola.pr.gov.br>
2026-03-30 10:48:44 -0300

    Create cont8.py

---
+++ conteudo/cont8.py
@@ -0,0 +1,12 @@
+BIBLIOTECA RANDOM (3 TEC)
+import random
+num = random.random()#computador vai gerar um número aleatório, entre 0 e um
+num2 = random.randint(1,10)
+print(num)
+print(num2)
+
+IMPORTAÇÃO DE BIBLIOTECAS
+Se digitar o comando import e ctrl+espaço conseguimos ver todas as bibliotecas disponíveis para usar no python.
+Vamos voltar para a página inicial do python.org e vamos no pypi. O py vem de python e o pi e de package index que 
+significa de pacotes extras, que são funcionalidades foras do python que podem ser utilizadas no python. Programadores 
+da comunidade podem publicar pacotes para outras pessoas utilizarem. Por exemplo eu posso fazer um módulo para verificar cpf.
