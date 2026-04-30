e4392d2b58edca744bca22c577c665ae55cc040c
silvacamposluana-cloud <silva.campos.luana@escola.pr.gov.br>
2026-04-30 08:52:39 -0300

    Update ex020.py

--- exercicio/ex020.py
+++ exercicio/ex020.py
@@ -1,8 +1,8 @@
-from.random.import.choice
+from random import choice
 aluno1=str(input('o primeiro aluno: '))
 aluno2=str(input('o segundo aluno: '))
 aluno3=str(input('o terceiro aluno: '))
 aluno4=str(input('o quarto aluno: '))
 lista= [aluno1,aluno2,aluno3,aluno4]
 ecolhido=choice(lista)#escolhe um da lista 
-print('o aluno escolhido foi {}'.format(escolhio))
+print('o aluno ecolhio foi {}'.format(ecolhido))
